abbd44a3ec3b66247cc4dea08fba7843699fcd44
sedtavud22 <phoosed@gmail.com>
2024-02-17 21:45:16 +0700

    My work integrate with data

--- src/features/account/components/MyWorkCard.jsx
+++ src/features/account/components/MyWorkCard.jsx
@@ -1,26 +1,31 @@
 import React from "react";
 import { CalendarIcon } from "../../../icons";
+import { format } from "date-fns";
+import { Link } from "react-router-dom";
 
-function MyWorkCard() {
+function MyWorkCard({ work }) {
   return (
     <div>
       <hr className="border mb-10" />
 
-      <div className="flex gap-8 items-center">
+      <Link className="flex gap-8 items-center" to={`/work/${work.id}`}>
         {/* Image */}
         <div className="w-36 aspect-[4/3] rounded-lg">
           <img
             className="aspect-[4/3] rounded-lg object-cover"
-            src="https://images.pexels.com/photos/1107807/pexels-photo-1107807.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            src={work?.cardImageUrl}
           />
         </div>
 
         {/* Info */}
         <div className="flex gap-2">
           <CalendarIcon className="w-6 h-6" />
-          <p>19/1/24 - 24/1/24</p>
+          <p>{`${format(work?.firstAvailableDate, "dd/MM/yy")} - ${format(
+            work?.lastAvailableDate,
+            "dd/MM/yy"
+          )}`}</p>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
--- src/features/account/components/MyWorkList.jsx
+++ src/features/account/components/MyWorkList.jsx
@@ -1,12 +1,17 @@
+import { useParams } from "react-router-dom";
+import useWork from "../../work/hooks/use-work";
 import MyWorkCard from "./MyWorkCard";
 
 function MyWorkList() {
+  const { userId } = useParams();
+  const { works } = useWork();
+  const myWorkArray = works.filter((work) => work.photographerId === +userId);
   return (
     <div className="col-span-8 flex flex-col gap-10">
       <h1 className="text-3xl font-semibold">My Works</h1>
-      <MyWorkCard />
-      <MyWorkCard />
-      <MyWorkCard />
+      {myWorkArray?.map((work) => (
+        <MyWorkCard key={work.id} work={work} />
+      ))}
     </div>
   );
 }
